418fe6ea4b8efb920a95e07d6c96fe36dd83deae
Daniel C <brikbusters@gmail.com>
2025-03-28 23:10:24 -0400

    Update tests to use ptp_new and ptp_close

--- test/test.c
+++ test/test.c
@@ -10,8 +10,6 @@
 int test_data(void);
 
 int test_setup_usb(struct PtpRuntime *r) {
-	ptp_init(r);
-
 	if (ptp_device_init(r)) {
 		puts("Device connection error");
 		return 1;
@@ -26,9 +24,9 @@ int test_setup_usb(struct PtpRuntime *r) {
 }
 
 int ptp_vcam_magic(void) {
-	struct PtpRuntime r;
+	struct PtpRuntime *r = ptp_new(PTP_USB);
 
-	int rc = test_setup_usb(&r);
+	int rc = test_setup_usb(r);
 	if (rc) return rc;
 
 	int sizes[] = {4, 10, 101, 513, 1000, 997, 257, 511, 1, 2, 3};
@@ -53,27 +51,27 @@ int ptp_vcam_magic(void) {
 
 		cmd.params[0] = checksum;
 
-		rc = ptp_send_data(&r, &cmd, buffer, sizes[i]);
+		rc = ptp_send_data(r, &cmd, buffer, sizes[i]);
 		if (rc) return rc;
 	}
 
-	rc = ptp_eos_evproc_run(&r, "EnableBootDisk %d 'aasd'", 10);
+	rc = ptp_eos_evproc_run(r, "EnableBootDisk %d 'aasd'", 10);
 	if (rc) return rc;
 
-	ptp_close(&r);
+	ptp_close(r);
 	return 0;
 }
 
 // Test case for EOS T6/1300D vcam
 int test_eos_t6(void) {
-	struct PtpRuntime r;
+	struct PtpRuntime *r = ptp_new(0);
 
 	struct PtpDeviceInfo di;
 
-	int rc = test_setup_usb(&r);
+	int rc = test_setup_usb(r);
 	if (rc) return rc;
 
-	rc = ptp_get_device_info(&r, &di);
+	rc = ptp_get_device_info(r, &di);
 	if (rc) return rc;
 
 	// In the very least, at least all of the standard PTP props should be logged
@@ -83,16 +81,16 @@ int test_eos_t6(void) {
 	ptp_device_info_json(&di, buffer, sizeof(buffer));
 	printf("%s\n", buffer);
 
-	if (ptp_device_type(&r) == PTP_DEV_EOS) {
-		ptp_eos_set_remote_mode(&r, 1);
-		ptp_eos_set_event_mode(&r, 1);
+	if (ptp_device_type(r) == PTP_DEV_EOS) {
+		ptp_eos_set_remote_mode(r, 1);
+		ptp_eos_set_event_mode(r, 1);
 	}
 
-	rc = ptp_eos_get_event(&r);
+	rc = ptp_eos_get_event(r);
 	if (rc) return rc;
 
 	char events_json[4096];
-	ptp_eos_events_json(&r, events_json, sizeof(events_json));
+	ptp_eos_events_json(r, events_json, sizeof(events_json));
 
 	printf("Events: %s\n", events_json);
 
@@ -101,37 +99,37 @@ int test_eos_t6(void) {
 	assert(!strcmp(di.model, "Canon EOS Rebel T6"));
 
 	struct PtpArray *arr;
-	rc = ptp_get_storage_ids(&r, &arr);
+	rc = ptp_get_storage_ids(r, &arr);
 	if (rc) return rc;
 	int id = arr->data[0];
 
 	assert(arr->length == 1); // vcam only has 1 fs
 
 	struct PtpStorageInfo si;
-	rc = ptp_get_storage_info(&r, arr->data[0], &si);
+	rc = ptp_get_storage_info(r, arr->data[0], &si);
 	if (rc) return rc;
 
 	ptp_storage_info_json(&si, buffer, sizeof(buffer));
 	printf("%s\n", buffer);
 
-	//ptp_device_close(&r);
-	ptp_close(&r);
+	ptp_device_close(r);
+	ptp_close(r);
 	return 0;
 }
 
 int test_props(void) {
-	struct PtpRuntime r;
+	struct PtpRuntime *r = ptp_new(PTP_USB);
 
-	int rc = test_setup_usb(&r);
+	int rc = test_setup_usb(r);
 	if (rc) return rc;
 
-	rc = ptp_get_prop_value(&r, PTP_DPC_BatteryLevel);
+	rc = ptp_get_prop_value(r, PTP_DPC_BatteryLevel);
 	if (rc) return rc;
-	printf("Test parse: %d\n", ptp_parse_prop_value(&r));
-	assert(ptp_parse_prop_value(&r) == 50);
+	printf("Test parse: %d\n", ptp_parse_prop_value(r));
+	assert(ptp_parse_prop_value(r) == 50);
 
 	struct PtpPropDesc pd;
-	rc = ptp_get_prop_desc(&r, PTP_DPC_BatteryLevel, &pd);
+	rc = ptp_get_prop_desc(r, PTP_DPC_BatteryLevel, &pd);
 	if (rc) return rc;
 	assert(pd.current_value32 == 50);
 	assert(pd.default_value32 == 50);
@@ -140,39 +138,39 @@ int test_props(void) {
 	assert(pd.range_form.max == 100);
 	assert(pd.range_form.step == 1);
 
-	rc = ptp_get_prop_desc(&r, PTP_DPC_ImageSize, &pd);
+	rc = ptp_get_prop_desc(r, PTP_DPC_ImageSize, &pd);
 	if (rc) return rc;
 
 	char buffer[128];
 	ptp_read_string(pd.default_value, buffer, sizeof(buffer));
 	assert(!strcmp(buffer, "640x480"));
 
-	ptp_close(&r);
+	ptp_close(r);
 	return 0;
 }
 
 int test_fs(void) {
-	struct PtpRuntime r;
+	struct PtpRuntime *r = ptp_new(PTP_USB);
 
-	int rc = test_setup_usb(&r);
+	int rc = test_setup_usb(r);
 	if (rc) return rc;
 
 	struct PtpDeviceInfo di;
-	rc = ptp_get_device_info(&r, &di);
+	rc = ptp_get_device_info(r, &di);
 	if (rc) return rc;
 	char buffer[4096];
 	ptp_device_info_json(&di, buffer, sizeof(buffer));
 	printf("%s\n", buffer);
 
 	struct PtpArray *arr;
-	rc = ptp_get_storage_ids(&r, &arr);
+	rc = ptp_get_storage_ids(r, &arr);
 	if (rc) return rc;
 	int id = arr->data[0];
 
 	assert(arr->length == 1); // vcam only has 1 fs
 
 	struct PtpStorageInfo si;
-	rc = ptp_get_storage_info(&r, arr->data[0], &si);
+	rc = ptp_get_storage_info(r, arr->data[0], &si);
 	if (rc) return rc;
 
 	ptp_storage_info_json(&si, buffer, sizeof(buffer));
@@ -181,17 +179,17 @@ int test_fs(void) {
 /* Not implemented in vcam!
 	struct PtpObjectInfo oi = {0};
 	strcpy(oi.filename, "test.jpg");
-	rc = ptp_send_object_info(&r, id, 0, &oi);
+	rc = ptp_send_object_info(r, id, 0, &oi);
 	if (rc) return rc;
 */
 
-	rc = ptp_get_object_handles(&r, id, 0, 0, &arr);
+	rc = ptp_get_object_handles(r, id, 0, 0, &arr);
 	if (rc) return rc;
-	assert(ptp_get_return_code(&r) == PTP_RC_OK);
+	assert(ptp_get_return_code(r) == PTP_RC_OK);
 
 	for (int i = 0; i < arr->length; i++) {
 		struct PtpObjectInfo oi;
-		rc = ptp_get_object_info(&r, arr->data[i], &oi);
+		rc = ptp_get_object_info(r, arr->data[i], &oi);
 		if (rc) return rc;
 
 		printf("Filename: %s\n", oi.filename);
@@ -199,14 +197,14 @@ int test_fs(void) {
 	}
 
 	struct PtpObjectInfo oi;
-	rc = ptp_get_object_info(&r, 0xdeadbeef, &oi);
+	rc = ptp_get_object_info(r, 0xdeadbeef, &oi);
 	if (rc) return rc;
 	assert(!strcmp(oi.filename, "test.png"));
 	assert(oi.compressed_size == 1234);
 
 	free(arr);
 
-	ptp_close(&r);
+	ptp_close(r);
 	return 0;
 }
 
@@ -239,14 +237,14 @@ static void *thread(void *arg) {
 }
 
 static int test_multithread(void) {
-	struct PtpRuntime r;
+	struct PtpRuntime *r = ptp_new(PTP_USB);
 
-	int rc = test_setup_usb(&r);
+	int rc = test_setup_usb(r);
 	if (rc) return rc;
 
 	for (int i = 0; i < 100; i++) {
 		pthread_t th;
-		if (pthread_create(&th, NULL, thread, (void *)&r) != 0) {
+		if (pthread_create(&th, NULL, thread, (void *)r) != 0) {
 			perror("pthread_create() error");
 			exit(1);
 		}
@@ -254,7 +252,8 @@ static int test_multithread(void) {
 
 	usleep(100000);
 
-	//ptp_device_close(&r);
+	ptp_device_close(r);
+	ptp_close(r);
 
 	return 0;	
 }
