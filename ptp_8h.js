a9ecd4998aa1012f447b37826cd808187ee71887
petabyt <petabyt@users.noreply.github.com>
2025-05-24 05:17:25 +0000

    deploy: 75368ace48ec9b7288062d6c52564bc726bb139d

--- ptp_8h.js
+++ ptp_8h.js
@@ -3,7 +3,7 @@ var ptp_8h =
     [ "PtpBulkContainer", "structPtpBulkContainer.html", null ],
     [ "PtpEventContainer", "structPtpEventContainer.html", null ],
     [ "PtpIpHeader", "structPtpIpHeader.html", null ],
-    [ "PtpIpBulkContainer", "structPtpIpBulkContainer.html", null ],
+    [ "PtpIpRequestContainer", "structPtpIpRequestContainer.html", null ],
     [ "PtpIpResponseContainer", "structPtpIpResponseContainer.html", null ],
     [ "PtpIpStartDataPacket", "structPtpIpStartDataPacket.html", null ],
     [ "PtpIpEndDataPacket", "structPtpIpEndDataPacket.html", null ],
