2c769c7b49657898054022bd2809f6b48d1f3408
petabyt <petabyt@users.noreply.github.com>
2025-06-05 20:38:32 +0000

    deploy: 3b0e84732606c671026f6d3dbd532075739cf814

--- cl__data_8h.js
+++ cl__data_8h.js
@@ -1,5 +1,6 @@
 var cl__data_8h =
 [
-    [ "ptp_eos_events_next", "group__Data.html#ga901f87c8c02f34ace292608c2b971521", null ],
+    [ "ptp_eos_events_json", "group__Data.html#gaf464eba46ed3dd3118bfb4dd3853acf5", null ],
+    [ "ptp_eos_events_next", "group__Data.html#ga174e0286b0f894339f85db3690433411", null ],
     [ "ptp_eos_events_open", "group__Data.html#ga18dab575bd8b6b0b136bc2bfe372779e", null ]
 ];
