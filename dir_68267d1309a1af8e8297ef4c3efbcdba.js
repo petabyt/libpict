666c0dbbfb9dc2e636c0f3811bf23a76f18687ee
petabyt <petabyt@users.noreply.github.com>
2026-08-07 17:37:22 +0000

    deploy: ebbda8fc06d39e57914d17e99aead569cc3cb64c

--- dir_68267d1309a1af8e8297ef4c3efbcdba.js
+++ dir_68267d1309a1af8e8297ef4c3efbcdba.js
@@ -6,5 +6,5 @@ var dir_68267d1309a1af8e8297ef4c3efbcdba =
     [ "cl_enum.h", "cl__enum_8h.html", null ],
     [ "cl_ops.h", "cl__ops_8h.html", "cl__ops_8h" ],
     [ "libpict.h", "libpict_8h.html", "libpict_8h" ],
-    [ "ptp.h", "ptp_8h.html", "ptp_8h" ]
+    [ "ptp.h", "ptp_8h.html", null ]
 ];
